4b22fd38c04b4775b070831d6b952ff63676c700
Arryuann Khanna <aryankhannachd@gmail.com>
2023-10-18 11:25:51 +0530

    first commit

---
+++ src/Featurestile/Fetaurestile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Featurestile.css'
+
+export const Fetaurestile = () => {
+  return (
+    <div className="featurestile-container">
+        <section className="featurestile-preview"></section>
+         <section className="featurestile-content-container"></section>
+    </div>
+  )
+}
+
+export default Fetaurestile
---
+++ src/LandingPage/Landingpage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './LandingPage.css'
+
+export const LandingPage = () => {
+  return (
+    <div className="landingpage">
+       <div className="landing-heading">
+        <p>Buy and Sell the useful goods</p>
+       </div>
+       <div className="landing-about">
+       ThaparBuddy is a devoted platform created to promote simple and effective purchase and exchange of old books and multiple other items within the college community.
+       </div>
+       <div className="landing-buttons">
+        <div className="landing-button1"></div>
+        <div className="landing-button2"></div>
+       </div>
+
+    </div>
+  )
+}
+
+export default LandingPage
