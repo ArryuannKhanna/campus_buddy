6ff67057e763f6deb86d8d38c148f1dab12be138
PHANTOMFALCON <vsawhney_be21@thapar.edu>
2023-11-08 15:18:26 +0530

    *

---
+++ src/FoundersReviews/FoundersReviews.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FoundersReviews.css'
+import Header from  './FoundersReviews/Header';
+import Content from  './FoundersReviews/Content';
+function foundersreviews()
+{
+    return(
+        <div className='container'>
+            <Header />
+            <Content />
+            </div>
+    )
+}
+export default foundersreviews;
