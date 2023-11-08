188f83638e58ed8cf16b1ba92a0ee4d715dfde11
PHANTOMFALCON <vsawhney_be21@thapar.edu>
2023-11-08 17:07:00 +0530

    not final but about to complete

--- src/FoundersReviews/FoundersReviews.js
+++ src/FoundersReviews/FoundersReviews.js
@@ -4,31 +4,17 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const FounderReviewsCarousel = () => {
-  const founderReviews = [
-    {
-      name: 'John Doe',
-      position: 'Founder & CEO',
-      review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      name: 'Jane Smith',
-      position: 'Co-Founder',
-      review: 'Praesent tincidunt quam eget neque congue, in tincidunt urna placerat.',
-    },
-    // Add more reviews as needed
-  ];
-
+const FounderReviewsCarousel = ({ founderReviews }) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4, // Display 4 items at once
+    slidesToShow: 2, // Display 2 items at once
     slidesToScroll: 1,
   };
 
   return (
-    <div className="founder-reviews-carousel" style={{ width: '60%', margin: '0 auto' }}>
+    <div className="founder-reviews-carousel" style={{ width: '80%', margin: '0 auto 20px' }}>
       <Slider {...settings}>
         {founderReviews.map((review, index) => (
           <div key={index} className="founder-review">
@@ -42,4 +28,41 @@ const FounderReviewsCarousel = () => {
   );
 };
 
-export default FounderReviewsCarousel;
+const App = () => {
+  const firstandsecondFounderReviews = [
+    {
+      name: 'John Doe',
+      position: 'Founder & CEO',
+      review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      name: 'Jane Smith',
+      position: 'Co-Founder',
+      review: 'Praesent tincidunt quam eget neque congue, in tincidunt urna placerat.',
+    },
+    // Add more reviews as needed
+  ];
+
+  const thirdandfourthFounderReviews = [
+    {
+      name: 'Alice Johnson',
+      position: 'CTO',
+      review: 'Vestibulum vel purus nec tortor egestas varius.',
+    },
+    {
+      name: 'Bob Williams',
+      position: 'Lead Developer',
+      review: 'Suspendisse potenti. Nulla facilisi.',
+    },
+    // Add more reviews as needed
+  ];
+
+  return (
+    <div>
+      <FounderReviewsCarousel founderReviews={firstandsecondFounderReviews} />
+      <FounderReviewsCarousel founderReviews={thirdandfourthFounderReviews} />
+    </div>
+  );
+};
+
+export default App;
