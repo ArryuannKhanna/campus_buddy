b837acaa5ba6ddd0d7fd8a85f7e257bc3adb9302
PHANTOMFALCON <vsawhney_be21@thapar.edu>
2023-11-08 15:43:27 +0530

    Update FoundersReviews.js

--- src/FoundersReviews/FoundersReviews.js
+++ src/FoundersReviews/FoundersReviews.js
@@ -1,8 +1,6 @@
 import './FoundersReviews.css'
 import React from 'react';
 import Slider from 'react-slick';
-
-// Import CSS styles for the carousel from slick-carousel
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -14,33 +12,23 @@ const FounderReviewsCarousel = () => {
       review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
-      name: 'Janne Smith',
+      name: 'Jane Smith',
       position: 'Co-Founder',
       review: 'Praesent tincidunt quam eget neque congue, in tincidunt urna placerat.',
     },
     // Add more reviews as needed
-    {
-      name: 'John Doe',
-      position: 'Founder & CEO',
-      review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      name: 'Janne Smith',
-      position: 'Co-Founder',
-      review: 'Praesent tincidunt quam eget neque congue, in tincidunt urna placerat.',
-    },
   ];
 
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4, // Display 4 items at once
     slidesToScroll: 1,
   };
 
   return (
-    <div className="founder-reviews-carousel">
+    <div className="founder-reviews-carousel" style={{ width: '60%', margin: '0 auto' }}>
       <Slider {...settings}>
         {founderReviews.map((review, index) => (
           <div key={index} className="founder-review">
